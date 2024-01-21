9492d7801d017e76474ab7dc1abf9ca9852011ef
rofinafiin <rofinafiisr@gmail.com>
2024-01-21 11:41:12 +0000

    repo and owner name to body

--- employee/employee.go
+++ employee/employee.go
@@ -11,7 +11,7 @@ import (
 	"os"
 )
 
-func GetDataCommitsAll(PublicKey, MongoEnv, dbname, colname, personalToken, reponame, ownername string, r *http.Request) string {
+func GetDataCommitsAll(PublicKey, MongoEnv, dbname, colname, personalToken string, r *http.Request) string {
 	req := new(structure.Creds)
 	conn := PasetoprojectBackend.MongoCreateConnection(MongoEnv, dbname)
 	var datauser structure.BodyReq
@@ -39,7 +39,7 @@ func GetDataCommitsAll(PublicKey, MongoEnv, dbname, colname, personalToken, repo
 				req.Status = http.StatusNotAcceptable
 				req.Message = "Data User tidak ada"
 			} else {
-				datacomms, err := githubwrapper.ListCommitALL(context.Background(), personalToken, reponame, ownername)
+				datacomms, err := githubwrapper.ListCommitALL(context.Background(), personalToken, datauser.RepoName, datauser.OwnerName)
 				if err != nil {
 					req.Status = http.StatusBadRequest
 					req.Message = err.Error()
